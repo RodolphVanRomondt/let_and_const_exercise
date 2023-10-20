3967df77e2d3306ddc221b7da5f2af5c89084e7e
Rodolph VAN ROMONDT <57846281+RodolphVanRomondt@users.noreply.github.com>
2023-10-19 23:45:58 -0700

    responded to questions about var, let and const

---
+++ let_const_exercise.js
@@ -0,0 +1,12 @@
+/* Write an ES2015 Version */
+const PI = 3.14;
+
+/* QUIZ */
+// you can't redeclare a variable with "let" and it has a block level scope
+
+// you can't reassigne a variable "const"
+
+// you can't reassigne a variable with "const" compare to "let"
+
+// "hoisting" is when a variable, declaring with "var",
+// is move to the top of is scope with a value of "undefined"
